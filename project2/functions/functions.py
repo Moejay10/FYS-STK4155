74c9ff636682390fe649b40c7c9b0ba56b45d0f3
Moejay10 <mohe95@hotmail.no>
2019-11-19 10:17:34 +0100

    Finalized projects

---
+++ project2/functions/functions.py
@@ -0,0 +1,124 @@
+
+import numpy as np
+
+
+def FrankeFunction(x,y):
+    """
+    Generates Franke's function.
+    Input:
+    Takes array x and y.
+    Output
+    Returns array z.
+    """
+
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+
+    return term1 + term2 + term3 + term4
+
+def generate_data(number_points = 20, plott = False):
+    """
+    Generates data.
+    Input:
+    plott = True for plotting.
+    Output:
+    returns 1D arrays x, y and z (after begin raveled).
+    """
+    x_data = np.arange(0, 1, 1./number_points)
+    y_data = np.arange(0, 1, 1./number_points)
+
+    x, y = np.meshgrid(x_data,y_data)
+
+    z = FrankeFunction(x, y)
+    if plott == True:
+        plotter(x,y,z)
+        plt.savefig('plots/Franke/frankefunction.pdf')
+
+    #flatten the matrix out
+    x = np.ravel(x)
+    y = np.ravel(y)
+    z = np.ravel(z)
+
+    print ("x ranges from", 0, "to", 1, "with a total amount of", number_points, "points.")
+    print ("y ranges from", 0, "to", 1, "with a total amount of", number_points, "points.")
+
+
+    eps = np.random.normal(0,1,len(z))
+    z += 0.1*eps
+
+    return x, y, z
+
+
+def find_designmatrix(x,y, polygrad=5):
+    """
+    Function:
+    Generates the designmatrix.
+    Input:
+    Takes an array x and y and a polynomial degree.
+    Output:
+    Returns a multidimensional array (designmatrix).
+    """
+    x2 = x*x
+    y2 = y*y
+    x3 = x*x*x
+    y3 = y*y*y
+
+    if (polygrad<1):
+        raise ValueError ("error! polygrad is less than 1!!")
+
+    if polygrad == 1:
+        X = np.c_[np.ones((len(x),1)),x, y] #3
+    elif (polygrad == 2):
+        X = np.c_[np.ones((len(x),1)), #0-degree polynomial
+                     x, y, #1-degree polynomial
+                     x2,y2,x*y] #2-degree polynomial #6
+    elif polygrad == 3:
+        X = np.c_[np.ones((len(x),1)), #0-degree polynomial
+                         x, y, #1-degree polynomial
+                         x2,y2,x*y, #2-degree polynomial
+                         x3,y3,x*y2,x2*y] #3-degree polynomial #10
+    elif polygrad == 4:
+        X = np.c_[np.ones((len(x),1)), #0-degree polynomial
+                         x, y, #1-degree polynomial
+                         x2,y2,x*y, #2-degree polynomial
+                         x3,y3,x*y2,x2*y, #3-degree polynomial
+                         x*x3,y*y3,x3*y,x*y3,x2*y2] #4-degree polynomial #15
+
+    elif polygrad ==5:
+        X = np.c_[np.ones((len(x),1)), #0-degree polynomial
+                     x, y, #1-degree polynomial
+                     x2,y2,x*y, #2-degree polynomial
+                     x3,y3,x*y2,x2*y, #3-degree polynomial
+                     x*x3,y*y3,x3*y,x*y3,x2*y2, #4-degree polynomial
+                     x3*x2,y3*y2,(x2*x2)*y, x*(y2*y2),x3*y2,x2*y3] #5-degree polynomial #21
+
+    #General formula to avoid hardcoding 'too' much.
+    elif (polygrad > 5):
+        X = np.zeros( (len(x), int(0.5*(polygrad + 2)*(polygrad + 1)) ) )
+        poly = 0
+        for i in range(int(polygrad) + 1):
+            for j in range(int(polygrad) + 1 - i):
+                X[:,poly] = np.squeeze((x**i)*(y**j))
+                poly += 1
+    return X
+
+
+
+
+def sigmoid(x):
+    return 1./(1.+np.exp(-x))
+def learning_schedule(t):
+    t0, t1 = 1, 50
+    return t0/(t+t1)
+
+def to_categorical_numpy(integer_vector):
+    n_inputs = len(integer_vector)
+    n_categories = np.max(integer_vector) + 1
+    onehot_vector = np.zeros((n_inputs, n_categories))
+    onehot_vector[range(n_inputs), integer_vector] = 1
+
+    return onehot_vector
+def accuracy_score_numpy(Y_test, Y_pred):
+    return np.sum(Y_test == Y_pred) / len(Y_test)
---
+++ project2/main.py
@@ -0,0 +1,184 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import  train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+from sklearn.linear_model import SGDRegressor
+from random import random, seed
+
+#import functions
+from functions.neuralnetwork import NeuralNetwork
+from NeuralNetwork_regression import NeuralNetwork2
+from functions.functions import *
+"""
+Simple filter for sklearn
+"""
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+
+cancer = load_breast_cancer()
+#print(len(cancer.target))
+
+# Set up training data
+X_train, X_test, y_train, y_test = train_test_split(cancer.data,cancer.target,random_state=0)
+
+
+# Scale data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+theta_linreg = np.linalg.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(y_train)
+#print("Own inversion")
+#print(theta_linreg)
+
+
+n = len(X_train)
+M = 10
+m = int(n/M)
+n_epochs = 150
+
+t0, t1 = 1, 50
+
+#remember to write about the choice of eta (learning rate).
+beta = np.random.randn(len(X_train[0,:]),1)
+
+costfunction_best = 100000
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_train[random_index:random_index+1]
+        yi = y_train[random_index:random_index+1]
+
+        gradients = 2 * xi.T @ ((xi @ beta)-yi)
+        eta = learning_schedule(epoch*m+i)
+        beta = beta - eta*gradients
+
+        Xbeta = X_train.dot(beta)
+
+        costfunction = y_train.dot( Xbeta ) - np.log( 1 + np.exp( Xbeta )  )
+        #print(costfunction)
+        costfunction = -np.sum(costfunction)
+
+        p = np.exp(Xbeta)/(1+np.exp(Xbeta))
+
+        if (costfunction) < (costfunction_best):
+            costfunction_best = costfunction
+            best_beta = beta
+
+#print("beta from own sdg")
+#print(best_beta, costfunction_best)
+
+
+y_tilde = X_test.dot(best_beta)
+
+I=0
+y_tilde = sigmoid(y_tilde)
+
+for i in range(len(y_tilde)):
+
+    if y_tilde[i]>=0.5:
+        y_tilde[i]=1
+    else:
+        y_tilde[i]=0
+
+    if y_tilde[i] == y_test[i]:
+        I += 1.
+#print (y_tilde)
+accuracy_score = float(I/len(y_tilde))
+print ("-------------------------------------------------")
+print ("--- Logistic Regression with Gradient Descent ---")
+print ("-------------------------------------------------")
+print ("Accuracy score on test set:", accuracy_score)
+
+"""
+sgdreg = SGDRegressor(max_iter = m, penalty=None, eta0=eta)
+sgdreg.fit(X_train,y_train)
+print("beta from sklearn sdg")
+#print(sgdreg.intercept_, sgdreg.coef_)
+"""
+
+"""
+#TEST AGAINST SKLEARN Logistic Regression
+#Logistic regression with sklearn
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+print("Test set accuracy: {:.2f}".format(logreg.score(X_test,y_test)))
+"""
+
+"""
+# remove this if scaling of dataset is not required.
+# Scale data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+logreg.fit(X_train_scaled, y_train)
+print("Test set accuracy scaled data: {:.2f}".format(logreg.score(X_test_scaled,y_test)))
+"""
+print()
+print ("-------------------------------------------------")
+print ("---------- Artificial Neural Network ------------")
+print ("-------------------------------------------------")
+# building our neural network
+n_inputs, n_features = X_train.shape
+n_hidden_neurons = 50
+n_categories = 2
+n_hidden_layers = 2
+
+epochs = 100
+batch_size = 100
+lmbd = 0.
+eta = 0.1
+
+y_train_onehot = to_categorical_numpy(y_train)
+
+dnn = NeuralNetwork(X_data = X_train,Y_data= y_train_onehot,n_hidden_layers=n_hidden_layers,
+                    n_hidden_neurons=n_hidden_neurons, n_categories=n_categories,
+                    epochs=epochs,batch_size=batch_size,eta=eta, lmbd=lmbd)
+dnn.train()
+test_predict = dnn.predict(X_test)
+# equivalent in numpy
+
+print("Accuracy score on test set:", accuracy_score_numpy(y_test, test_predict))
+
+
+
+#print("Accuracy score on test set: ", accuracy_score_numpy(Y_test, test_predict))
+
+
+
+print()
+print ("-------------------------------------------------")
+print ("---------- Artificial Neural Network ------------")
+print ("-------------------------------------------------")
+# building our neural network
+x, y, z = generate_data()
+
+x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x,y,z,test_size=0.2)
+X_train = find_designmatrix(x_train, y_train, polygrad=5)
+n_inputs, n_features = X_train.shape
+n_hidden_neurons = 100
+n_categories = 1
+n_hidden_layers = 2
+
+epochs = 100
+batch_size = 100
+lmbd = 0.0
+eta = 0.1
+
+#y_train_onehot = to_categorical_numpy(y_train)
+
+X_test = find_designmatrix(x_test, y_test, polygrad=5)
+dnn = NeuralNetwork2(X_data = X_train,Y_data= y_train,
+                    n_hidden_neurons=n_hidden_neurons, n_categories=n_categories,
+                    epochs=epochs,batch_size=batch_size,eta=eta, lmbd=lmbd)
+dnn.train()
+test_predict = dnn.predict(X_test)
+# equivalent in numpy
+
+print("Accuracy score on test set:", accuracy_score_numpy(z_test, test_predict))
